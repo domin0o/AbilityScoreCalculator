014a825080717b3610408523b5c2e6344e078ca5
Dominik Skrzydlewski <skrzydlewskijunior@gmail.com>
2023-01-28 15:01:42 +0100

    Utworzenie metod ReadInt oraz ReadDouble - finalne działanie programu.

--- AbilityScoreCalculator/Program.cs
+++ AbilityScoreCalculator/Program.cs
@@ -20,7 +20,7 @@ namespace AbilityScoreCalculator
             double divided = RollResult / DivideBy;
 
             // Dodawanie AddAmount do wyniku dzielenia
-            int added = AddAmount += (int)divided;
+            int added = AddAmount + (int)divided;
 
             // Jeśli wynik jest za niski, użyj Minimum
             if(added < Minimum)
@@ -32,24 +32,66 @@ namespace AbilityScoreCalculator
                 Score = added;
             }
         }
-
-        ///<summary>
-        /// Wyświetla informację i wczytuje wartość typu int z konsoli
-        ///</summary>
-        ///
-        static int ReadInt(int lastUsedValue, string prompt)
-        {
-            // Wyświetlanie informacji i [wartości domyślnej]:
-            // Wczytywanie wiersza dancyh wyjściowych i używanie int.TryParse do próby ich przetworzenia. Jeśli to możliwe, wyświetlanie w konsoli
-            // " użycie wartości " + value. W przeciwnym razie wysweitlanie w konsoli
-            // " użycie wartośći domyślnej " + lastUsedValue.
-        }
     }
     internal class Program
     {
+        /// <summary>
+        /// Wyświetla informację i wczytuje wartości typu int z konsoli
+        /// </summary>
+        /// <param name="lastUsedValue">Wartość domyślna</param>
+        /// <param name="prompt">Informacja wyświetlana w konsoli</param>
+        /// <returns>Wczytana wartość int lub wartość domyślna (jeśli nie można przetworzyć wartości domyślnej)</returns>
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            // Wyświetlanie informacji i [wartości domyślnej]:
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            // Wczytywanie wiersza dancyh wyjściowych i używanie int.TryParse do próby ich przetworzenia. Jeśli to możliwe, wyświetlanie w konsoli
+            string userAnswer = Console.ReadLine();
+            // " użycie wartości " + value. W przeciwnym razie wysweitlanie w konsoli
+            // " użycie wartośći domyślnej " + lastUsedValue.
+            if (int.TryParse(userAnswer, out int userInt) && userInt != lastUsedValue)
+            {
+                Console.WriteLine("Użycie wartości: " + userInt);
+                return userInt;
+            }
+            else
+            {
+                Console.WriteLine("Użycie wartości domyślnej: " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+
+        /// <summary>
+        /// Wyświetla informację i wczytuje wartości typu double z konsoli
+        /// </summary>
+        /// <param name="lastUsedValue">Wartość domyślna</param>
+        /// <param name="prompt">Informacja wyświetlana w konsoli</param>
+        /// <returns>Wczytana wartość double lub wartość domyślna (jeśli nie można przetworzyć wartości domyślnej)</returns>
+        static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            // Wyświetlanie informacji i [wartości domyślnej]:
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            // Wczytywanie wiersza dancyh wyjściowych i używanie double.TryParse do próby ich przetworzenia. Jeśli to możliwe, wyświetlanie w konsoli
+            string userAnswer = Console.ReadLine();
+            // " użycie wartości " + value. W przeciwnym razie wysweitlanie w konsoli
+            // " użycie wartośći domyślnej " + lastUsedValue.
+            if (double.TryParse(userAnswer, out double userInt) && userInt != lastUsedValue)
+            {
+                Console.WriteLine("Użycie wartości: " + userInt);
+                return userInt;
+            }
+            else
+            {
+                Console.WriteLine("Użycie wartości domyślnej: " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
         static void Main(string[] args)
         {
-
+            float f1 = 185.26F;
+            double d2 = 0.0000316D;
+            decimal m3 = 37.26M;
+            Console.WriteLine((int)f1 +" "+ (int)d2 +" "+ (int)m3);
             AbilityScoreCalculator calculator = new AbilityScoreCalculator();
             while (true)
             {
